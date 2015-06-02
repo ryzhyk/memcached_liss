51431ccb1d8ff0e19ed1655340df64f0f739a969
Leonid Ryzhyk <leonidr@cse.unsw.edu.au>
2015-06-02 15:28:44 -0400

    initial trace analysis script

--- lttng.sh
+++ lttng.sh
@@ -3,7 +3,10 @@
 lttng create memcached
 lttng enable-event -u 'memcached:*'
 lttng add-context -u -t perf:thread:cpu-cycles -t pthread_id
+str=$(lttng list memcached | sed -n 2p)
+dir=${str:16}
 lttng start
 ./memcached -m 256 -p 11211 -t 4
 lttng stop
 lttng destroy memcached
+python3 analyze_trace.py $dir/ust/uid/$UID/$(getconf LONG_BIT)-bit/
