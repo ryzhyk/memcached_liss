749a3805eab9e07213a82f35e609733362f5238d
Leonid Ryzhyk <leonidr@cse.unsw.edu.au>
2015-06-02 12:47:46 -0400

    add cycle counts and tid's to the trace

--- lttng.sh
+++ lttng.sh
@@ -2,6 +2,7 @@
 
 lttng create memcached
 lttng enable-event -u 'memcached:*'
+lttng add-context -u -t perf:thread:cpu-cycles -t pthread_id
 lttng start
 ./memcached -m 256 -p 11211 -t 4
 lttng stop
